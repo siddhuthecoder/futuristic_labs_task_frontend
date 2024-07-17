564b79c143fe9b676a6043812076eca3d42108f7
sribabu <sribabumandraju@gmail.com>
2024-07-17 23:44:47 +0530

    toggle bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 import Sidebar from "../components/Sidebar";
 import Canvas from "../components/Canvas";
 import styled from "styled-components";
+import { IoClose } from "react-icons/io5";
+import { PiSquaresFourFill } from "react-icons/pi";
 
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -17,12 +19,7 @@ const Home = () => {
   const [components, setComponents] = useState([]);
   const userData = useSelector((state) => state.user.user);
   const navigate = useNavigate();
-
-  // useEffect(() => {
-  //   if (!userData) {
-  //     navigate("/login");
-  //   }
-  // }, [userData, navigate]);
+  const [show, setShow] = useState(true);
 
   useEffect(() => {
     if (!userData) {
@@ -33,7 +30,33 @@ const Home = () => {
   return (
     <DndProvider backend={HTML5Backend}>
       <AppContainer>
-        <div className="w-[250px] h-screen overflow-y-scroll relative flex">
+        {!show && (
+          <span
+            className="z-[4] text-white cursor-pointer absolute left-[20px] w-[60px] h-[60px] border-[5px] border-[green] rounded-full bg-black flex justify-center items-center top-[20px]"
+            onClick={() => setShow(true)}
+          >
+            <PiSquaresFourFill className="text-4xl text-green-500 cursor-pointer" />
+          </span>
+          
+        )}
+        {show && (
+          <span
+            className="z-[4] text-white cursor-pointer absolute left-[220px] top-[10px]"
+            onClick={() => setShow(false)}
+          >
+            <IoClose className="text-3xl text-red-500 "/>
+          </span>
+          
+        )}
+        <div className={`w-[250px] h-screen ${show ? "flex" : "hidden"} flex-col overflow-y-scroll relative`}>
+          <div className="w-full flex items-center flex-row-reverse relative overflow-x-visible">
+            {/* <span
+              className="z-[4] text-white hidden md:block cursor-pointer"
+              onClick={() => setShow(false)}
+            >
+              <IoClose className="text-3xl text-red-500 "/>
+            </span> */}
+          </div>
           <Sidebar />
         </div>
         <Canvas components={components} setComponents={setComponents} />
