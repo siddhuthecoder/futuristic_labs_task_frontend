d270a08add0d7214a9027558342f47b127be59e0
siddhu_from_sklm <codewithsiddhu@gmialo.com>
2024-07-18 11:48:14 +0530

    SDf

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,13 +6,33 @@ import Canvas from "../components/Canvas";
 import styled from "styled-components";
 import { IoClose } from "react-icons/io5";
 import { PiSquaresFourFill } from "react-icons/pi";
-
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const AppContainer = styled.div`
   display: flex;
   height: 100vh;
+`;
+
+const MobileMessage = styled.div`
+  display: none;
+  @media (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.8);
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 5;
+    font-size: 1.5rem;
+    text-align: center;
+    padding: 1rem;
+  }
 `;
 
 const Home = () => {
@@ -30,6 +50,9 @@ const Home = () => {
   return (
     <DndProvider backend={HTML5Backend}>
       <AppContainer>
+        <MobileMessage>
+          Please open in desktop mode to use all features.
+        </MobileMessage>
         {!show && (
           <span
             className="z-[4] text-white cursor-pointer absolute left-[20px] w-[60px] h-[60px] border-[5px] border-[green] rounded-full bg-black flex justify-center items-center top-[20px]"
@@ -37,16 +60,14 @@ const Home = () => {
           >
             <PiSquaresFourFill className="text-4xl text-green-500 cursor-pointer" />
           </span>
-          
         )}
         {show && (
           <span
             className="z-[4] text-white cursor-pointer absolute left-[220px] top-[10px]"
             onClick={() => setShow(false)}
           >
-            <IoClose className="text-3xl text-red-500 "/>
+            <IoClose className="text-3xl text-red-500 " />
           </span>
-          
         )}
         <div className={`w-[250px] h-screen ${show ? "flex" : "hidden"} flex-col overflow-y-scroll relative`}>
           <div className="w-full flex items-center flex-row-reverse relative overflow-x-visible">
